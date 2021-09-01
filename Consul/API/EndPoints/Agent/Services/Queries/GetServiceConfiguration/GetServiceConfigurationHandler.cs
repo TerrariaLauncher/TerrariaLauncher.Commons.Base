cff11fa079b8a16b806e490d1388e451df08f40b
truongchauhien <truongchauhien@gmail.com>
2021-09-02 02:42:23 +0700

    Added a consul service query.

---
+++ Consul/API/EndPoints/Agent/Services/Queries/GetServiceConfiguration/GetServiceConfigurationHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using TerrariaLauncher.Commons.Consul.API.Commons;
+using TerrariaLauncher.Commons.Consul.API.DTOs;
+
+namespace TerrariaLauncher.Commons.Consul.API.EndPoints.Agent.Services.Queries.GetServiceConfiguration
+{
+    public class GetServiceConfigurationHandler : ConsulQueryHandler<GetServiceConfigurationQuery, GetServiceConfigurationResult>
+    {
+        protected override Task PrepareRequest(HttpRequestMessage httpRequestMessage, GetServiceConfigurationQuery query, CancellationToken cancellationToken = default)
+        {
+            var uri = new UriBuilder(httpRequestMessage.RequestUri);
+            uri.Path += $"{query.ServiceId}";
+            httpRequestMessage.RequestUri = uri.Uri;
+            return Task.CompletedTask;
+        }
+
+        protected override async Task<GetServiceConfigurationResult> ProcessResponse(HttpResponseMessage httpResponseMessage, GetServiceConfigurationQuery query, CancellationToken cancellationToken = default)
+        {
+            httpResponseMessage.EnsureSuccessStatusCode();
+            return new GetServiceConfigurationResult()
+            {
+                Service = await httpResponseMessage.Content.ReadFromJsonAsync<RegisteredService>(cancellationToken: cancellationToken)
+            };
+        }
+    }
+}
---
+++ Consul/API/EndPoints/Agent/Services/Queries/GetServiceConfiguration/GetServiceConfigurationQuery.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using TerrariaLauncher.Commons.Consul.API.Commons;
+using TerrariaLauncher.Commons.Consul.API.DTOs;
+
+namespace TerrariaLauncher.Commons.Consul.API.EndPoints.Agent.Services.Queries.GetServiceConfiguration
+{
+    public class GetServiceConfigurationQuery : ConsulQuery
+    {
+        public GetServiceConfigurationQuery(GetServiceConfigurationQueryOptions options) : base(options) { }
+        public GetServiceConfigurationQuery() : this(new GetServiceConfigurationQueryOptions()) { }
+
+        public string ServiceId { get; set; }
+    }
+
+    public class GetServiceConfigurationQueryOptions : ConsulQueryOptions
+    {
+        public override BlockingOptions Blocking => new BlockingOptions(true);
+        public override ConsistencyOptions Consistency => new ConsistencyOptions(false);
+        public override AgentCachingOptions AgentCaching => new AgentCachingOptions(AgentCachingForm.None);
+        public override HttpOptions Http => new HttpOptions("agent/service/", HttpMethod.Get);
+    }
+
+    public class GetServiceConfigurationResult : ConsulQueryResult
+    {
+        public RegisteredService Service { get; set; }
+    }
+}
